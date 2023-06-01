98d7632852c7ddec358b74a933cc42e00be1592c
Bhagirath Saxena <72344025+rix4uni@users.noreply.github.com>
2023-06-01 21:58:05 +0530

    Update test-hsdfgf.py

--- test-hsdfgf.py
+++ test-hsdfgf.py
@@ -18,3 +18,78 @@ GOOGLE_API_TOKEN = 'AIzaSyDDnc-68uaF-E4gzvJslt1fppiUl9dyTrc'
 FULLCONTACT_API_KEY = 'a82ad9009f6b1b1'
 WHATCMS_ACCESS_TOKEN = '756ab2cfa1ed5575a71e0714ef05c2e228f17b6b1476de7075f7f4d6b4978272376fb3'
 ghp_OoPp1t14LHdCeHUTOPEDuz96avTver2IlF39
+
+
+password: root
+password=test
+password = gjghjy5
+password => tegfhjg
+password: "nbvxvjpa8cgx"
+'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC'
+"support":"support@rix4uni.com"
+"rootDomain":"rix4uni.com"
+"username":"admin"
+"password":"admin123"
+userName = "0a99b2327b0ac0ee4a5fc92611348965"
+password = "773ecc24297c533de592f7769f05c33c"
+private_key = "idiots.pem"
+
+ssh_address:"80.225.90.78"
+ssh_port:9e3
+ssh_user:"root"
+
+u1 = User.create!( email: 'jackpack@noblecheetah.io', password: 'password')
+{ ip: "p.webshare.io", port: 80, username: "ownudlvd-1", password: "nbvxvjpa8cgx" }
+
+ssh_address=admincentos@145.100.58.235
+SSH_ADDRESS="10.11.99.1"
+SET PASSWORD= PASSWORD('Pass!@34w0rD');
+
+client = MongoClient('54.210.240.10', 27010)
+ssh_address = "ubuntu@ec2-54-234-221-196.compute-1.amazonaws.com"
+ssh_port = 27010
+mongo_username = "nelsonyang"
+mongo_password = "nelneltime"
+
+SSH_ADDRESS="10.11.99.1"
+const SSH_USERNAME = "testsftp"
+SSH_PASSWORD = "maker"
+
+host: 'sftp.grupokobsa.com.pe'
+port: '22'
+username: 'magnavoz'
+password: 'L7jdby7-8KJF'
+
+host: "smtp.gmail.com"
+port: 465
+user: 'teamworkproject10@gmail.com'
+pass: '10teamworkproject10'
+
+host: "sandbox.smtp.mailtrap.io"
+port: 2525
+user: "485195af2b267e"
+pass: "f8563df690eb91"
+
+host: "smtp.gmail.com"
+port: 465
+user: "nodemailtest0451@gmail.com"
+pass: "obecdbodoifhctvo"
+
+secret: 'servecodewithloginonlyprivilege2023'
+Host: "smtp.elasticemail.com"
+Username: "rammroy99@gmail.com"
+Password: "23FCA58CBC34041855F04463694414B4426B"
+
+ftp_host = '172.16.18.249'
+ftp_user = 'view'
+ftp_passwd = 'umessage'
+
+server = "ftp.5700.network"
+username = "dsouzaco"
+password = "GPjeOfuAJi7dQ0nYTpUI"
+
++17473771245 (United States)
++33756490775 (France)
++3197010517154 (Netherlands)
++447893920404 (United Kingdom)
++32468799982 (Belgium)
